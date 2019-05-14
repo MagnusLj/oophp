96a08066b068659dc731a892f639192219551ba1
MagnusLj <magnusljungvall@hotmail.com>
2019-05-15 00:03:22 +0100

    Movie database starting page done

---
+++ router/movie.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Create routes using $app programming style.
+ */
+//var_dump(array_keys(get_defined_vars()));
+
+
+
+/**
+ * Show all movies.
+ */
+// $app->router->get("movie", function () use ($app) {
+//     $title = "Movie database | oophp";
+//
+//     $app->db->connect();
+//     $sql = "SELECT * FROM movie;";
+//     $res = $app->db->executeFetchAll($sql);
+//
+//     $app->page->add("movie/index", [
+//         "res" => $res,
+//     ]);
+//
+//     return $app->page->render([
+//         "title" => $title,
+//     ]);
+// });
+
+$app->router->get("movie", function () use ($app) {
+    $data = [
+        "title" => "Movie database || oophp",
+    ];
+
+
+    $app->db->connect();
+
+    $sql = "SELECT * FROM movie;";
+    $resultset = $app->db->executeFetchAll($sql);
+
+    $data["resultset"] = $resultset;
+
+    $app->view->add("movie/index", $data);
+    $app->page->render($data);
+});
--- view/pig/debug.php
+++ view/pig/debug.php
@@ -3,22 +3,11 @@
 namespace Anax\View;
 
 /**
- * Render content within an article.
+ * Render a view.
  */
 
 // Show incoming variables and view helper functions
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
 
 
-?><hr>
-<pre>
-SESSION
-<?= var_dump($_SESSION) ?>
-Pigobj
-<?= var_dump($pigobj) ?>
-POST
-<?= var_dump($_POST) ?>
-GET
-<?= var_dump($_GET) ?>
-</pre>
-<hr>
+?>
