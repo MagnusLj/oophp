b32f6d83af581f69b4d9cfa809dcce0fe2dc433f
MagnusLj <magnusljungvall@hotmail.com>
2019-05-15 22:50:25 +0100

    Title and year search functioning

--- router/movie.php
+++ router/movie.php
@@ -73,6 +73,23 @@ $app->router->get("search-title", function () use ($app) {
 
     $app->page->add("movie/search-title", $data);
 
+    if ($searchTitle) {
+        $app->db->connect();
+        $sql = "SELECT * FROM movie WHERE title LIKE ?;";
+        $resultset = $app->db->executeFetchAll($sql, [$searchTitle]);
+        $app->page->add("movie/index", [
+            "resultset" => $resultset,
+        ]);
+}
+
+
+// $app->db->connect();
+// $sql = "SELECT * FROM movie;";
+// $resultset = $app->db->executeFetchAll($sql);
+
+
+
+
     return $app->page->render([
         "title" => $title,
     ]);
@@ -98,19 +115,114 @@ $app->router->post("search-title", function () use ($app) {
     return $app->response->redirect("search-title");
 });
 
-// $app->router->get("movie", function () use ($app) {
-//     $data = [
-//         "title" => "Movie database || oophp",
-//     ];
-//
-//
-//     $app->db->connect();
-//
-//     $sql = "SELECT * FROM movie;";
-//     $resultset = $app->db->executeFetchAll($sql);
-//
-//     $data["resultset"] = $resultset;
-//
-//     $app->view->add("movie/index", $data);
-//     $app->page->render($data);
-// });
+
+
+
+
+$app->router->get("search-year", function () use ($app) {
+    $title = "Movie database | oophp";
+    // $year1 = 1900;
+    // $year2 = 2100;
+    // if (isset($_POST)) {
+    //     print_r($_POST);
+    // }
+
+    // if ($_SESSION["doSearch"] ?? false) {
+    //     $searchTitle = $_SESSION["searchTitle"];
+    //     echo "Search titeeele";
+    //     print_r($_SESSION);
+    //     print_r($searchTitle);
+    //     }
+
+    $year1 = null;
+    $year2 = null;
+
+    if ($_SESSION["year1"] ?? false) {
+        $year1 = $_SESSION["year1"];
+        // print_r(year1);
+}
+
+    if ($_SESSION["year2"] ?? false) {
+        $year2 = $_SESSION["year2"];
+        // print_r(year2);
+    }
+
+    $data = [];
+
+    // $app->db->connect();
+    // $sql = "SELECT * FROM movie;";
+    // $resultset = $app->db->executeFetchAll($sql);
+    // $searchTitle = $_POST["searchTitle"] ?? null;
+
+
+
+
+
+
+    $data = [
+        "year1" => $year1,
+        "year2" => $year2
+        // "result" => $result ?? null
+    ];
+
+    // print_r($_POST);
+    // print_r($data);
+    // print_r($_SESSION);
+    // print_r($searchTitle);
+
+
+    $app->page->add("movie/header");
+
+    $app->page->add("movie/search-year", $data);
+
+    if ($year1 && $year2) {
+            $app->db->connect();
+            $sql = "SELECT * FROM movie WHERE year >= ? AND year <= ?;";
+            $resultset = $app->db->executeFetchAll($sql, [$year1, $year2]);
+            $app->page->add("movie/index", [
+                "resultset" => $resultset,
+            ]);
+        } elseif ($year1) {
+            $app->db->connect();
+            $sql = "SELECT * FROM movie WHERE year >= ?;";
+            $resultset = $app->db->executeFetchAll($sql, [$year1]);
+            $app->page->add("movie/index", [
+                "resultset" => $resultset,
+            ]);
+        } elseif ($year2) {
+            $app->db->connect();
+            $sql = "SELECT * FROM movie WHERE year <= ?;";
+            $resultset = $app->db->executeFetchAll($sql, [$year2]);
+            $app->page->add("movie/index", [
+                "resultset" => $resultset,
+            ]);
+        }
+
+        return $app->page->render([
+            "title" => $title,
+        ]);
+    });
+
+
+
+
+
+$app->router->post("search-year", function () use ($app) {
+    $title = "Movie database | oophp";
+
+
+    // $app->db->connect();
+    // $sql = "SELECT * FROM movie;";
+    // $resultset = $app->db->executeFetchAll($sql);
+
+    if ($_POST["doSearch"] ?? false) {
+        $_SESSION["year1"] = $_POST["year1"];
+        $_SESSION["year2"] = $_POST["year2"];
+        }
+
+    // return $app->page->render([
+    //     "title" => $title,
+    // ]);
+
+    return $app->response->redirect("search-year");
+});
--- view/movie/header.php
+++ view/movie/header.php
@@ -13,7 +13,7 @@
     <a href="?">Show all movies</a> |
     <a href="?route=reset">Reset database</a> |
     <a href="search-title">Search title</a> |
-    <a href="?route=search-year">Search year</a> |
+    <a href="search-year">Search year</a> |
     <a href="?route=movie-select">Select</a> |
 <!--    <a href="?route=movie-edit">Edit</a> | -->
     <a href="?route=show-all-sort">Show all sortable</a> |
