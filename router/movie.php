8715840339ed1a415adcff3e87afd9b563fd6915
MagnusLj <magnusljungvall@hotmail.com>
2019-05-16 20:30:53 +0100

    Done with kmom05 functionality and validation etc

--- router/movie.php
+++ router/movie.php
@@ -35,38 +35,18 @@ $app->router->get("movie", function () use ($app) {
 $app->router->get("search-title", function () use ($app) {
     $title = "Movie database | oophp";
     $searchTitle = "";
-    // if (isset($_POST)) {
-    //     print_r($_POST);
-    // }
 
-    // if ($_SESSION["doSearch"] ?? false) {
-    //     $searchTitle = $_SESSION["searchTitle"];
-    //     echo "Search titeeele";
-    //     print_r($_SESSION);
-    //     print_r($searchTitle);
-    //     }
 
     if ($_SESSION["searchTitle"] ?? false) {
         $searchTitle = $_SESSION["searchTitle"];
-        // print_r($searchTitle);
-}
+    }
 
     $data = [];
 
-    // $app->db->connect();
-    // $sql = "SELECT * FROM movie;";
-    // $resultset = $app->db->executeFetchAll($sql);
-    // $searchTitle = $_POST["searchTitle"] ?? null;
 
     $data = [
         "searchTitle" => $searchTitle
-        // "result" => $result ?? null
     ];
-
-    // print_r($_POST);
-    // print_r($data);
-    // print_r($_SESSION);
-    // print_r($searchTitle);
 
 
     $app->page->add("movie/header");
@@ -80,14 +60,7 @@ $app->router->get("search-title", function () use ($app) {
         $app->page->add("movie/index", [
             "resultset" => $resultset,
         ]);
-}
-
-
-// $app->db->connect();
-// $sql = "SELECT * FROM movie;";
-// $resultset = $app->db->executeFetchAll($sql);
-
-
+    }
 
 
     return $app->page->render([
@@ -100,17 +73,11 @@ $app->router->post("search-title", function () use ($app) {
     $title = "Movie database | oophp";
 
 
-    // $app->db->connect();
-    // $sql = "SELECT * FROM movie;";
-    // $resultset = $app->db->executeFetchAll($sql);
 
     if ($_POST["doSearch"] ?? false) {
         $_SESSION["searchTitle"] = $_POST["searchTitle"];
-        }
+    }
 
-    // return $app->page->render([
-    //     "title" => $title,
-    // ]);
 
     return $app->response->redirect("search-title");
 });
@@ -121,54 +88,25 @@ $app->router->post("search-title", function () use ($app) {
 
 $app->router->get("search-year", function () use ($app) {
     $title = "Movie database | oophp";
-    // $year1 = 1900;
-    // $year2 = 2100;
-    // if (isset($_POST)) {
-    //     print_r($_POST);
-    // }
 
-    // if ($_SESSION["doSearch"] ?? false) {
-    //     $searchTitle = $_SESSION["searchTitle"];
-    //     echo "Search titeeele";
-    //     print_r($_SESSION);
-    //     print_r($searchTitle);
-    //     }
 
     $year1 = null;
     $year2 = null;
 
     if ($_SESSION["year1"] ?? false) {
         $year1 = $_SESSION["year1"];
-        // print_r(year1);
-}
+    }
 
     if ($_SESSION["year2"] ?? false) {
         $year2 = $_SESSION["year2"];
-        // print_r(year2);
     }
 
     $data = [];
 
-    // $app->db->connect();
-    // $sql = "SELECT * FROM movie;";
-    // $resultset = $app->db->executeFetchAll($sql);
-    // $searchTitle = $_POST["searchTitle"] ?? null;
-
-
-
-
-
-
     $data = [
         "year1" => $year1,
         "year2" => $year2
-        // "result" => $result ?? null
     ];
-
-    // print_r($_POST);
-    // print_r($data);
-    // print_r($_SESSION);
-    // print_r($searchTitle);
 
 
     $app->page->add("movie/header");
@@ -176,32 +114,32 @@ $app->router->get("search-year", function () use ($app) {
     $app->page->add("movie/search-year", $data);
 
     if ($year1 && $year2) {
-            $app->db->connect();
-            $sql = "SELECT * FROM movie WHERE year >= ? AND year <= ?;";
-            $resultset = $app->db->executeFetchAll($sql, [$year1, $year2]);
-            $app->page->add("movie/index", [
-                "resultset" => $resultset,
-            ]);
-        } elseif ($year1) {
-            $app->db->connect();
-            $sql = "SELECT * FROM movie WHERE year >= ?;";
-            $resultset = $app->db->executeFetchAll($sql, [$year1]);
-            $app->page->add("movie/index", [
-                "resultset" => $resultset,
-            ]);
-        } elseif ($year2) {
-            $app->db->connect();
-            $sql = "SELECT * FROM movie WHERE year <= ?;";
-            $resultset = $app->db->executeFetchAll($sql, [$year2]);
-            $app->page->add("movie/index", [
-                "resultset" => $resultset,
-            ]);
-        }
+        $app->db->connect();
+        $sql = "SELECT * FROM movie WHERE year >= ? AND year <= ?;";
+        $resultset = $app->db->executeFetchAll($sql, [$year1, $year2]);
+        $app->page->add("movie/index", [
+            "resultset" => $resultset,
+        ]);
+    } elseif ($year1) {
+        $app->db->connect();
+        $sql = "SELECT * FROM movie WHERE year >= ?;";
+        $resultset = $app->db->executeFetchAll($sql, [$year1]);
+        $app->page->add("movie/index", [
+            "resultset" => $resultset,
+        ]);
+    } elseif ($year2) {
+        $app->db->connect();
+        $sql = "SELECT * FROM movie WHERE year <= ?;";
+        $resultset = $app->db->executeFetchAll($sql, [$year2]);
+        $app->page->add("movie/index", [
+            "resultset" => $resultset,
+        ]);
+    }
 
         return $app->page->render([
             "title" => $title,
         ]);
-    });
+});
 
 
 
@@ -210,24 +148,13 @@ $app->router->get("search-year", function () use ($app) {
 $app->router->post("search-year", function () use ($app) {
     $title = "Movie database | oophp";
 
-
-    // $app->db->connect();
-    // $sql = "SELECT * FROM movie;";
-    // $resultset = $app->db->executeFetchAll($sql);
-
     if ($_POST["doSearch"] ?? false) {
         $_SESSION["year1"] = $_POST["year1"];
         $_SESSION["year2"] = $_POST["year2"];
-        }
-
-    // return $app->page->render([
-    //     "title" => $title,
-    // ]);
+    }
 
     return $app->response->redirect("search-year");
 });
-
-
 
 
 
@@ -238,10 +165,6 @@ $app->router->get("movie-select", function () use ($app) {
     $app->db->connect();
     $sql = "SELECT * FROM movie;";
     $movies = $app->db->executeFetchAll($sql);
-
-    // $pigobj = new Malm18\Pig\Pig();
-    //
-    // $pigobj->gozer();
 
     $app->page->add("movie/header");
 
@@ -262,66 +185,41 @@ $app->router->post("movie-select", function () use ($app) {
     if ($_POST["doEdit"] ?? false && is_numeric($movieId)) {
         $_SESSION["movieId"] = $_POST["movieId"];
         return $app->response->redirect("movie-edit");
-        }
-
-    elseif ($_POST["doAdd"] ?? false) {
+    } elseif ($_POST["doAdd"] ?? false) {
         $app->db->connect();
         $sql = "INSERT INTO movie (title, year, image) VALUES (?, ?, ?);";
         $app->db->execute($sql, ["A title", 2017, "img/noimage.png"]);
         $_SESSION["movieId"] = $app->db->lastInsertId();
         return $app->response->redirect("movie-edit");
-        }
-
-    elseif ($_POST["doDelete"] ?? false) {
+    } elseif ($_POST["doDelete"] ?? false) {
         $movieId = $_POST["movieId"];
         $app->db->connect();
         $sql = "DELETE FROM movie WHERE id = ?;";
         $app->db->execute($sql, [$movieId]);
         return $app->response->redirect("movie");
-        }
-
+    }
 });
-
-// elseif (getPost("doAdd")) {
-//             $sql = "INSERT INTO movie (title, year, image) VALUES (?, ?, ?);";
-//             $db->execute($sql, ["A title", 2017, "img/noimage.png"]);
-//             $movieId = $db->lastInsertId();
-//             header("Location: ?route=movie-edit&movieId=$movieId");
-//             exit;
-
 
 
 
 $app->router->get("movie-edit", function () use ($app) {
     $title = "Movie database | oophp";
 
-
-
-    // $app->db->connect();
-    // $sql = "SELECT * FROM movie;";
-    // $movies = $app->db->executeFetchAll($sql);
-
-    // $pigobj = new Malm18\Pig\Pig();
-    //
-    // $pigobj->gozer();
-
     if ($_SESSION["movieId"] ?? false) {
         $movieId = $_SESSION["movieId"];
-        // print_r(year1);
-}
+    }
 
-// print_r($_SESSION);
 
-if ($movieId) {
-    $app->db->connect();
-    $sql = "SELECT * FROM movie WHERE id LIKE ?;";
-    $movie = $app->db->executeFetchAll($sql, [$movieId]);
-    print_r($movie[0]->title);
-    $movie = $movie[0];
-    // $app->page->add("movie/index", [
-    //     "resultset" => $resultset,
-    // ]);
-}
+    if ($movieId) {
+        $app->db->connect();
+        $sql = "SELECT * FROM movie WHERE id LIKE ?;";
+        $movie = $app->db->executeFetchAll($sql, [$movieId]);
+        print_r($movie[0]->title);
+        $movie = $movie[0];
+        // $app->page->add("movie/index", [
+        //     "resultset" => $resultset,
+        // ]);
+    }
 
     $app->page->add("movie/header");
 
@@ -347,16 +245,6 @@ $app->router->post("movie-edit", function () use ($app) {
         $app->db->connect();
         $sql = "UPDATE movie SET title = ?, year = ?, image = ? WHERE id = ?;";
         $app->db->execute($sql, [$movieTitle, $movieYear, $movieImage, $movieId]);
-        return $app->response->redirect("movie-edit");
-        }
+        return $app->response->redirect("movie");
+    }
 });
-
-
-
-
-// if (getPost("doSave")) {
-//     $sql = "UPDATE movie SET title = ?, year = ?, image = ? WHERE id = ?;";
-//     $db->execute($sql, [$movieTitle, $movieYear, $movieImage, $movieId]);
-//     header("Location: ?route=movie-edit&movieId=$movieId");
-//     exit;
-// }
--- view/pig/debug.php
+++ view/pig/debug.php
@@ -3,11 +3,22 @@
 namespace Anax\View;
 
 /**
- * Render a view.
+ * Render content within an article.
  */
 
 // Show incoming variables and view helper functions
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
 
-
-?>
+// $computer = $this->app->session->get("computer", null);
+?><hr>
+<pre>
+SESSION
+<?= var_dump($_SESSION) ?>
+Pigobj
+<?= var_dump($pigobj) ?>
+POST
+<?= var_dump($_POST) ?>
+GET
+<?= var_dump($_GET) ?>
+</pre>
+<hr>
